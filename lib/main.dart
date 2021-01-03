c84722a724e77f7a073259446824e2cd01641d7d
Pranav Nair <n.pranav@iitg.ac.in>
2021-01-03 17:06:53 +0530

    added files

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,9 +16,46 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return Container();
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int LeftDiceNumber =1;
+  int RightDiceNumber =1;
+  void ChangeDiceFace(){
+    LeftDiceNumber = Random().nextInt(6) + 1;
+    RightDiceNumber = Random().nextInt(6) + 1;
+  }
+  @override
+  Widget build(BuildContext context){
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: FlatButton(
+              onPressed: (){
+                setState(() {
+                  ChangeDiceFace();
+                });
+              },
+              child: Image.asset('images/dice$LeftDiceNumber.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: (){
+                setState(() {
+                  ChangeDiceFace();
+                });
+              },
+              child: Image.asset('images/dice$RightDiceNumber.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
+
